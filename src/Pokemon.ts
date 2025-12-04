8aa0a7765af76455787363f9db150d2067b97975
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2025-12-04 17:38:39 -0600

    Crear pokemon a partir de JSON

--- src/Pokemon.ts
+++ src/Pokemon.ts
@@ -11,5 +11,12 @@ export class Pokemon {
     this.Tipos = tipos;
   }
 
+  public static FromJson(pokeJson: any): Pokemon {
+    const id = pokeJson["id"];
+    const nombre = pokeJson["name"];
+    const spriteUrl =
+      pokeJson["sprites"]["other"]["official-artwork"]["front_default"];
+    const tipos = pokeJson["types"].map((tipo: any) => tipo["type"]["name"]);
+    return new Pokemon(id, nombre, spriteUrl, tipos);
   }
 }
