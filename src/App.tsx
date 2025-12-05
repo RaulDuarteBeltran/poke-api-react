0dc2df265b09801c3880584e15f75a2475726746
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2025-12-04 18:52:12 -0600

    Agregar Select para pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,26 @@ import "./App.css";
 import PokeCarta from "./PokeCarta";
 
 function App() {
-  return <PokeCarta id={35} />;
+  const [selectedPokemonId, setSelectedPokemonId] = useState<number>(35);
+  return (
+    <>
+      <select
+        onChange={(event) => {
+          const selectedValue = event.target.value;
+          const parsedValue = Number.parseInt(selectedValue);
+          setSelectedPokemonId(parsedValue);
+        }}
+        value={selectedPokemonId}
+      >
+        <option value={35}>Clefairy</option>
+        <option value={387}>Tortuig</option>
+        <option value={397}>Staravia</option>
+        <option value={493}>Arceus</option>
+        <option value={6}>Charizard</option>
+      </select>
+      <PokeCarta id={selectedPokemonId} />;
+    </>
+  );
 }
 
 export default App;
