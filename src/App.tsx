1caf145f74d6e409c25ceeaa62af8738a5d3b874
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2025-12-04 18:39:58 -0600

    Obtener un pokemon con la PokeAPI

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,18 @@ import "./App.css";
 import PokeCarta from "./PokeCarta";
 
 function App() {
-  return <PokeCarta id={35} />;
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  //   fetch("https://pokeapi.co/api/v2/pokemon/35")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const clefairy = Pokemon.FromJson(data);
+  //       console.log(clefairy);
+  //       setPokemons([clefairy]);
+  //     });
+
+  if (pokemons.length === 0) return <h1>No hay pokemons :(</h1>;
+  return <PokeCarta pokemon={pokemons[0]} />;
 }
 
 export default App;
